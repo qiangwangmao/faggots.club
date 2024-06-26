d29d3f693a0ecd5519111205f1cf3318291aa047
qiangwangmao <uss03word792fdx@gmail.com>
2024-06-26 13:53:52 +0200

    sd

---
+++ script.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const text = "xiorn";
+    const speed = 200; // Typing speed in milliseconds
+    const pause = 2000; // Pause before deleting and re-typing in milliseconds
+    const h1 = document.querySelector(".typewriter-text");
+
+    let i = 0;
+    let isDeleting = false;
+
+    function type() {
+        let displayText = text.substring(0, i);
+        h1.textContent = displayText;
+
+        if (!isDeleting && i < text.length) {
+            i++;
+            setTimeout(type, speed);
+        } else if (isDeleting && i > 0) {
+            i--;
+            setTimeout(type, speed);
+        } else if (!isDeleting && i === text.length) {
+            isDeleting = true;
+            setTimeout(type, pause);
+        } else if (isDeleting && i === 0) {
+            isDeleting = false;
+            setTimeout(type, speed);
+        }
+    }
+
+    type();
+});
